f9b0e2015c87e3e2c52d8c9dd75f88a76825b09d
DennisNgrox <dennisdasilva04@gmail.com>
2025-03-11 09:40:32 -0300

    Atualização do APP Python instrumentado

--- create_api.py
+++ create_api.py
@@ -1,18 +1,3 @@
-version: '3.7'
-services:
-  app:
-    build:
-      dockerfile: ./Dockerfile
-    ports:
-      - "5000:5000"
-    restart: unless-stopped
-    networks:
-      - local_default
-
-networks:
-  local_default:
-    external: true
-[root@pop-os api_python]# cat create_api.py 
 from flask import Flask, jsonify, request
 import json
 import shutil
@@ -105,4 +90,4 @@ def deletar_dado(id):
 
 
 if __name__ == "__main__":
-    app.run(port=5000, host="0.0.0.0"
+    app.run(port=5000, host="0.0.0.0")
